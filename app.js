4e99aa8b7f056ba1fd3bd6f6514db4e1c928da99
DavidS <david_k_2811@hotmail.com>
2025-02-23 15:52:40 -0300

    Crear función para capturar valor de campo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables
+
+let amigosArray = [];
+
+//Funciones
+
+function agregarAmigo(params) {
+    let amigosArray = document.getElementById('amigo').value;
+    
+}
+
